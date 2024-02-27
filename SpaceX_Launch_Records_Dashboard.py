9649ce6335e5f7f16ce995f588ea637f38c9408c
imranchy <63488646+imranchy@users.noreply.github.com>
2024-02-27 10:10:19 +0000

    Update SpaceX_Launch_Records_Dashboard.py

--- SpaceX_Launch_Records_Dashboard.py
+++ SpaceX_Launch_Records_Dashboard.py
@@ -4,7 +4,8 @@ import pandas as pd
 import plotly.express as px
 
 # Read the airline data into pandas dataframe
-spacex_df = pd.read_csv("spacex_launch_dash.csv")
+dataset = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv"
+spacex_df = pd.read_csv(dataset)
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
