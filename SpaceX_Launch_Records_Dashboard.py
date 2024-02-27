7b1ee74353e45538fbb7c56b6e2d9b042d76aa6f
imranchy <63488646+imranchy@users.noreply.github.com>
2024-02-27 09:57:50 +0000

    Update SpaceX_Launch_Records_Dashboard.py
    Needed further changes.

--- SpaceX_Launch_Records_Dashboard.py
+++ SpaceX_Launch_Records_Dashboard.py
@@ -1,9 +1,6 @@
 # Import required libraries
+from dash import Dash, dcc, html, Input, Output
 import pandas as pd
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.dependencies import Input, Output
 import plotly.express as px
 
 # Read the airline data into pandas dataframe
@@ -12,7 +9,7 @@ max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
 # Create a dash application
-app = dash.Dash(__name__)
+app = Dash(__name__)
 server = app.server
 
 # Create an app layout
